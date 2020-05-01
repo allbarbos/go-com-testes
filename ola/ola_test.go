752f5c1a51024886040b48eef63be9e5ee216a10
Allan Barbosa <allbarbos13@gmail.com>
2020-05-01 19:22:19 -0300

    trativa para nome vazio

--- ola/ola_test.go
+++ ola/ola_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Birl")
-	esperado := "Olá, Birl"
-
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper() // é necessário para dizermos ao conjunto de testes que este é um método auxiliar
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Chris")
+		esperado := "Olá, Chris"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("'Mundo' como padrão para 'string' vazia", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
 }
